5cebf282639becb1db7258d4a89fd51d0b98763a
Bar0ti <89021944+Bar0ti@users.noreply.github.com>
2021-08-30 01:36:52 +0200

    Bunker 1-3 submission

--- 106.py
+++ 106.py
@@ -1,28 +1,13 @@
-# Bunker 1-2 7.38 made by Bar0ti https://youtu.be/lvmWrBAX0TU
-# High chance of improving it, I haven't tested out all of the combinations of elevator entry
-#also the moment when I throw the bottle isn't optimized, it's very likely delaying the earliest possible
-#moment at which I can intereact with the button
-#A possible change to the whole strat is to use both bottles to kill both the rickies, this will allow for a longer
-#ricky boost, however to achieve this one will have to try fiddling with the position from which they execute the ricky boost.
+# Bunker 1-2 6.80 made by Bar0ti https://youtu.be/do5WBTN9rII
+# Possibly can be improved upon, you will have to figure out whether adding more slashes in here will be faster
 
 from katas import Controller, Scripter
-
 
 script = Scripter()
 script.Button(Controller.left_shoulder, duration=0)
 script.Wait(2)
-script.Button(Controller.down, duration=1)
-script.Wait(1)
 script.Roll()
-script.Wait(17)
-script.Button(Controller.X, duration=1)
-script.Wait(4)
-script.Button(Controller.down, duration=1)
-script.Wait(1)
-script.Roll()
-script.Wait(16)
-script.Button(Controller.X, duration=1)
-script.Wait(5)
+script.Wait(21)
 script.Button(Controller.down, duration=1)
 script.Wait(1)
 script.Roll()
@@ -30,37 +15,61 @@ script.Wait(21)
 script.Button(Controller.down, duration=1)
 script.Wait(1)
 script.Roll()
-script.Wait(2)
-script.Button(Controller.X, duration=1)
-script.Wait(23)
+script.Wait(16)
+script.Button(Controller.X, duration=0)
+script.Wait(16)
 script.Button(Controller.down, duration=1)
 script.Wait(1)
 script.Roll()
-script.Wait(13)
-script.Button(Controller.right, duration=10)
-script.Wait(2)
-script.MoveLeftStick(25)
-script.Button(Controller.X, duration=1)
-script.Wait(20)
-script.MoveLeftStick(330)
-script.Button(Controller.X, duration=1)
-script.Wait(3)
-script.Wait(2)
+script.Wait(21)
 script.Button(Controller.down, duration=1)
 script.Wait(1)
 script.Roll()
-script.Wait(4)
-script.MoveLeftStick(180)
-script.Button(Controller.B, duration=1)
-script.Wait(15)
+script.Wait(21)
+script.Button(Controller.down, duration=1)
+script.Wait(1)
+script.Roll()
+script.Wait(1)
 script.Button(Controller.Y, duration=1)
-script.Wait(3)
-script.Button(Controller.down, duration=1)
-script.Wait(1)
-script.Roll()
-script.Wait(5)
-script.Button(Controller.X, duration=1)
-
-
-
+script.Wait(12)
+script.Button(Controller.X, duration=0)
 script.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1
