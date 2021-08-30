fe7406572558b6f82a530803b3dd233e5ffada57
Bar0ti <89021944+Bar0ti@users.noreply.github.com>
2021-08-30 02:13:14 +0200

    bunker 1-4 submission

---
+++ 108.py
@@ -0,0 +1,77 @@
+# Bunker 1-4 6.02 made by Bar0ti https://youtu.be/W4m3TjAg-T8
+# Maybe it can be improved upon 
+
+from katas import Controller, Scripter
+
+script = Scripter()
+script.Button(Controller.left_shoulder, duration=0)
+script.Wait(2)
+script.Button(Controller.down, duration=1)
+script.Wait(1)
+script.Roll()
+script.Wait(7)
+script.Button(Controller.X, duration=1)
+script.Wait(3)
+script.Button(Controller.down, duration=1)
+script.Wait(1)
+script.Roll()
+script.Wait(10)
+script.Button(Controller.X, duration=1)
+script.Wait(4)
+script.Button(Controller.down, duration=1)
+script.Wait(1)
+script.Roll()
+script.Wait(19)
+script.Button(Controller.X, duration=1)
+script.Wait(4)
+script.Button(Controller.down, duration=1)
+script.Wait(1)
+script.Roll()
+script.Wait(21)
+script.Button(Controller.down, duration=1)
+script.Wait(1)
+script.Roll()
+script.Wait(2)
+script.Button(Controller.Y, duration=0)
+script.Wait(12)
+script.Button(Controller.X, duration=0)
+script.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1
