24538cb92789f363a41a8c689096350fc55cc1ff
Bar0ti <89021944+Bar0ti@users.noreply.github.com>
2022-08-03 01:53:54 +0200

    Add files via upload

--- 31.py
+++ 31.py
@@ -1,22 +1,75 @@
-# Prison 1 - 12.17 made by kc0rtez https://streamable.com/ljdv29
-# Unlikely to be improved unless scuffed rolls are explored 
-from katas import Scripter, Controller
+# Prison-1 4.77 + 7.23 made by Bar0ti https://youtu.be/J9nW0Vw8j2s
+# Find a new rolling method to be faster :)
+# the timing method is bit inaccurate, it's probably off by a few frames
 
+from katas import Controller, Scripter
 script = Scripter()
-script.Button(Controller.A)
-script.Wait(60*7) # This is innacurate, should be counted in actual frames instead
-script.Button(Controller.down, duration=10)
-script.Wait(3)
-script.Button(Controller.X)
-script.AddRawAction(437, 3, rt=0xFF) # the waits should align to this
-script.Wait(1)
 
-for _ in range(13):
-    script.Wait(10)
-    script.Button(Controller.X)
+
+script.Button(Controller.left_shoulder, 0)
+script.Wait(2)
+script.Button(Controller.down, duration=1)
+script.Wait(1)
+script.Roll()
+script.Wait(10)
+for k in range(7):
+    script.MoveLeftStick(0.8)
+    script.Button(Controller.X, duration=0)
     script.Wait(11)
-    script.Button(Controller.down, duration=9)
+    script.Button(Controller.down, duration=1)
     script.Wait(1)
     script.Roll()
-
+    script.Wait(10)
+    script.Button(Controller.left_thumb, duration=0)
+    script.Wait(3)
+    script.Button(Controller.down, duration=1)
+    script.Wait(1)
+    script.Roll()
+    script.Wait(8)
+script.MoveLeftStick(0)
+script.Button(Controller.X, duration=0)
 script.save()
+
+#slow-mo
+#script.Button(Controller.left_thumb, duration=0)
+
+#c-slash timings
+#script.Button(Controller.down, duration=1)
+#script.Wait(1)
+#script.Roll()
+#script.Wait(10)
+#script.MoveLeftStick(0)
+#script.Button(Controller.X, duration=0)
+#script.Wait(11)
+
+#scuffed slow-mo c-slashing timings
+#script.MoveLeftStick(0.8)
+#script.Button(Controller.X, duration=0)
+#script.Wait(11)
+#script.Button(Controller.down, duration=1)
+#script.Wait(1)
+#script.Roll()
+#script.Wait(10)
+#script.Button(Controller.left_thumb, duration=0)
+#script.Wait(3)
+#script.Button(Controller.down, duration=1)
+#script.Wait(1)
+#script.Roll()
+#script.Wait(10)
+
+#slow-mo roll cancel timings
+#script.Button(Controller.down, duration=1)
+#script.Wait(1)
+#script.Roll()
+#script.Wait(11)
+#script.Button(Controller.right, duration=6)
+#script.Button(Controller.A, duration=0)
+#script.Button(Controller.left_thumb, duration=0)
+#script.Wait(2)
+#script.Button(Controller.down, duration=1)
+#script.Wait(3)
+#script.Button(Controller.down, duration=1)
+#script.Wait(1)
+#script.Roll()
+
+
