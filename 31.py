bb2d37b16f55a75a9d5e30454342ff6ad9ca408c
Bar0ti <89021944+Bar0ti@users.noreply.github.com>
2022-09-03 19:33:19 +0200

    Add files via upload

--- 31.py
+++ 31.py
@@ -1,6 +1,6 @@
-# Prison-1 4.77 + 7.23 made by Bar0ti https://youtu.be/J9nW0Vw8j2s
-# Find a new rolling method to be faster :)
-# the timing method is bit inaccurate, it's probably off by a few frames
+# Prison-1 made by Bar0ti https://youtu.be/ZWigbCOMRqs
+# Maybe improvable
+# Play around with roll lengths and stuff
 
 from katas import Controller, Scripter
 script = Scripter()
@@ -12,26 +12,23 @@ script.Button(Controller.down, duration=1)
 script.Wait(1)
 script.Roll()
 script.Wait(10)
-for k in range(7):
-    script.MoveLeftStick(0.8)
+for k in range (7):
+    script.MoveLeftStick(0.6)
     script.Button(Controller.X, duration=0)
     script.Wait(11)
     script.Button(Controller.down, duration=1)
     script.Wait(1)
     script.Roll()
-    script.Wait(10)
+    script.Wait(8)
     script.Button(Controller.left_thumb, duration=0)
     script.Wait(3)
-    script.Button(Controller.down, duration=1)
+    script.Button(Controller.down, duration=0)
     script.Wait(1)
     script.Roll()
-    script.Wait(8)
+    script.Wait(10)
 script.MoveLeftStick(0)
 script.Button(Controller.X, duration=0)
 script.save()
-
-#slow-mo
-#script.Button(Controller.left_thumb, duration=0)
 
 #c-slash timings
 #script.Button(Controller.down, duration=1)
@@ -43,16 +40,16 @@ script.save()
 #script.Wait(11)
 
 #scuffed slow-mo c-slashing timings
-#script.MoveLeftStick(0.8)
+#script.MoveLeftStick(0.6)
 #script.Button(Controller.X, duration=0)
 #script.Wait(11)
 #script.Button(Controller.down, duration=1)
 #script.Wait(1)
 #script.Roll()
-#script.Wait(10)
+#script.Wait(8)
 #script.Button(Controller.left_thumb, duration=0)
 #script.Wait(3)
-#script.Button(Controller.down, duration=1)
+#script.Button(Controller.down, duration=0)
 #script.Wait(1)
 #script.Roll()
 #script.Wait(10)
@@ -66,10 +63,27 @@ script.save()
 #script.Button(Controller.A, duration=0)
 #script.Button(Controller.left_thumb, duration=0)
 #script.Wait(2)
-#script.Button(Controller.down, duration=1)
-#script.Wait(3)
+#script.Button(Controller.down, duration=0)
+#script.Wait(2)
 #script.Button(Controller.down, duration=1)
 #script.Wait(1)
 #script.Roll()
 
+#Roll cancel on stairs
+#script.Button(Controller.right, duration=7)
+#script.Button(Controller.A, duration=0)
+#script.Button(Controller.left_thumb, duration=0)
+#script.Wait(3)
+#script.Button(Controller.down, duration=2)
+#script.Wait(1)
+#script.Roll()
 
+#Early wall slide exit into a c-roll
+#script.Button(Controller.left, duration=6)
+#script.Wait(6)
+#script.Button(Controller.right, duration=6)
+#script.Button(Controller.down, duration=0)
+#script.Wait(2)
+#script.Button(Controller.down, duration=1)
+#script.Wait(1)
+#script.Roll()
